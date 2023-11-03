a296d13ea8d997e62ce8fb5fe2e9b689425984b9
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-02 22:10:33 -0700

    Time: 935 ms (32.71%) | Memory: 16.7 MB (57.35%) - LeetSync

---
+++ 2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maximumDetonation(self, bombs: List[List[int]]) -> int:
+        def will_detonate(bomb1, bomb2) -> bool:
+            return bomb1[2] >= math.sqrt( (bomb1[0]-bomb2[0])**2 + (bomb1[1]-bomb2[1])**2 )
+        
+        nearby = { i:[ j for j, bomb2 in enumerate(bombs) if will_detonate(bomb, bomb2[:2])] for i, bomb in enumerate(bombs) }
+        max_detonate = 1
+        for i, bomb in enumerate(bombs):
+            detonated = [False] * len(bombs)
+            count = 0
+            queqe = [ i ]
+            while queqe:
+                j = queqe.pop()
+                if detonated[j]: continue
+                detonated[j] = True
+                count += 1
+                queqe.extend(nearby[j])
+            if count > max_detonate: max_detonate= count
+
+        return max_detonate
+
+
+
