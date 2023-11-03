88b554d0526a58cae56d51f6d46f3538b9bed7a5
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-02 20:06:43 -0700

    Time: 35 ms (86.67%) | Memory: 16.2 MB (44.85%) - LeetSync

--- 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.py
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.py
@@ -2,9 +2,8 @@ class Solution:
     def buildArray(self, target: List[int], n: int) -> List[str]:
         res = []
         old = 0
-        unit = ["Push", "Pop"]
         for n in target:
-            if n > old + 1: res.extend( unit * (n-old-1))
+            if n > old + 1: res.extend( ["Push", "Pop"] * (n-old-1))
             res.append("Push")
             old = n
         return res
