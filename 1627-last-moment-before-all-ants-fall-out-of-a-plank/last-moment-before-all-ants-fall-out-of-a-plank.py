494199c0850eb86276bdab541eebf59a8a011cc4
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-04 01:16:22 -0700

    Time: 143 ms (82.01%) | Memory: 17.2 MB (67.63%) - LeetSync

--- 1627-last-moment-before-all-ants-fall-out-of-a-plank/last-moment-before-all-ants-fall-out-of-a-plank.py
+++ 1627-last-moment-before-all-ants-fall-out-of-a-plank/last-moment-before-all-ants-fall-out-of-a-plank.py
@@ -1,8 +1,9 @@
 class Solution:
     def getLastMoment(self, n: int, left: List[int], right: List[int]) -> int:
         maxtime=0
-        for t in itertools.chain(left, [n-x for x in right]):
-            if t > maxtime: maxtime = t
+        for a,b  in itertools.zip_longest(left, right, fillvalue = -1):
+            if a!= -1 and a > maxtime: maxtime = a
+            if b!=-1 and  n-b > maxtime: maxtime = n-b
         return maxtime
 
         
