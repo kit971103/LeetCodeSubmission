d357b0f8e1ab6abfc71ed57efebe4f8e2c208f36
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-04 01:43:32 -0700

    Time: 304 ms (26.43%) | Memory: 28.9 MB (42.86%) - LeetSync

--- 2706-minimum-score-by-changing-two-elements/minimum-score-by-changing-two-elements.py
+++ 2706-minimum-score-by-changing-two-elements/minimum-score-by-changing-two-elements.py
@@ -1,5 +1,4 @@
 class Solution:
     def minimizeSum(self, nums: List[int]) -> int:
-        if len(nums) < 4 : return 0
         nums.sort()
         return min( nums[-1] - nums[2], nums[-2] - nums[1], nums[-3] - nums[0])
