258102b08f6dd09df29b50d13886c9a78c0f32ce
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-04 22:44:25 -0700

    Time: 198 ms (70.66%) | Memory: 16.1 MB (98.11%) - LeetSync

--- 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.py
+++ 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.py
@@ -1,5 +1,6 @@
 class Solution:
     def distributeCookies(self, cookies: List[int], k: int) -> int:
+        
         n = len(cookies)
         if n == k: return max(cookies)
         if n == k+1: 
@@ -10,18 +11,13 @@ class Solution:
 
         for base_case in itertools.combinations(range(n), k):
 
-            remainning_cookies = [cookies[i] for i in range(n) if i not in base_case]
-            base_case = [ cookies[i] for i in base_case ]
+            remainning_cookies = [ i for i in range(n) if i not in base_case]
 
             for distribution in itertools.product( range(k) , repeat = n-k ):
                 
-                children = base_case.copy()
-                for i, cookiesLabel in enumerate(distribution):  children[cookiesLabel] += remainning_cookies[i]
-                
+                children = [ cookies[cookies_index1] + sum([ cookies[ remainning_cookies[cookies_index2] ] for cookies_index2 ,child_index2 in enumerate(distribution) if child_index2 ==  child_index1]) for child_index1, cookies_index1 in enumerate(base_case)]
+
                 unfairness = max(children)
                 if unfairness < min_unfairness : min_unfairness = unfairness
-
-
-            print()
                 
         return min_unfairness
