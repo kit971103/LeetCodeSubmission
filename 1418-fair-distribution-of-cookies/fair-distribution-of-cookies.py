ccfa3c121e5358ca455ee08faf8453b3bb19cabb
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-04 22:10:38 -0700

    Time: 47 ms (88.56%) | Memory: 16.3 MB (58.28%) - LeetSync

--- 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.py
+++ 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.py
@@ -1,9 +1,14 @@
 class Solution:
     def distributeCookies(self, cookies: List[int], k: int) -> int:
+        n = len(cookies)
+        if n == k: return max(cookies)
+        if n == k+1: 
+            cookies.sort()
+            return max(cookies[0] + cookies[1], cookies[-1])
         
         def distribute_ith_cookie(i):
             nonlocal min_unfairness
-            if i == len(cookies):
+            if i == n:
                 min_unfairness = min(min_unfairness, max(children))
                 return
             if len(children) < k:
@@ -12,6 +17,9 @@ class Solution:
                 children.pop()
             for j in range(len(children)):
                 children[j] += cookies[i]
+                # if children[j] >= min_unfairness:
+                #     children[j] -= cookies[i]
+                #     return
                 distribute_ith_cookie(i+1)
                 children[j] -= cookies[i]
 
