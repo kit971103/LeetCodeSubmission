8351160f282f06a72091fbf4f108e084f4572fd5
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-04 23:12:00 -0700

    Time: 530 ms (61.96%) | Memory: 30.2 MB (23.37%) - LeetSync

--- 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.py
+++ 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.py
@@ -1,14 +1,15 @@
 class Solution:
     def getWinner(self, arr: List[int], k: int) -> int:
         if k >= len(arr)-1: return max(arr)
+        if  k == 1: return max(arr[0], arr[1])
         winner = arr[0]
         win_count = 0
         for n in arr[1:]:
             if winner > n: 
                 win_count+=1
+                if win_count == k: break
             else:
                 winner = n
                 win_count =1
-            if win_count == k: break
         return winner
         
