92085bd37218f10a84f1bdeb99674599ba67a934
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-05 21:54:24 -0800

    Time: 397 ms (92.23%) | Memory: 42.4 MB (98.79%) - LeetSync

--- 1955-seat-reservation-manager/seat-reservation-manager.py
+++ 1955-seat-reservation-manager/seat-reservation-manager.py
@@ -1,26 +1,15 @@
 class SeatManager:
 
     def __init__(self, n: int):
-        # self.reservation = [Flase] * n
         self.freeseaat = []
         self.reservation_number = 0
-        self.max_reservation = n
-        
 
     def reserve(self) -> int:
         if self.freeseaat:
             return heapq.heappop(self.freeseaat)
-        elif self.reservation_number < self.max_reservation:
+        else:
             self.reservation_number += 1
             return self.reservation_number
-        else:
-            return -1
+
     def unreserve(self, seatNumber: int) -> None:
         heapq.heappush(self.freeseaat, seatNumber)
-        
-
-
-# Your SeatManager object will be instantiated and called as such:
-# obj = SeatManager(n)
-# param_1 = obj.reserve()
-# obj.unreserve(seatNumber)
