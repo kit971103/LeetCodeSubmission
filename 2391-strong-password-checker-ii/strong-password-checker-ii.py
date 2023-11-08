312da5f633096640b1ab1cc5a374fda1662c6838
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-07 23:38:34 -0800

    Time: 30 ms (96.39%) | Memory: 16.1 MB (95.33%) - LeetSync

--- 2391-strong-password-checker-ii/strong-password-checker-ii.py
+++ 2391-strong-password-checker-ii/strong-password-checker-ii.py
@@ -4,17 +4,14 @@ class Solution:
         if len(password) < 8: return False
 
         haveUpper = haveLower = haveDigit = haveSpecChar  = False
-        Lower = "qwertyuiopasdfghjklzxcvbnm"
-        Upper = "QWERTYUIOPASDFGHJKLZXCVBNM"
-        Digit = "1234567890"
         SpecChar = "!@#$%^&*()-+"
         last_char = None
         
         for char in password:
             if char == last_char: return False
-            elif not haveUpper and char in Upper: haveUpper = True
-            elif not haveLower and char in Lower: haveLower = True
-            elif not haveDigit and char in Digit: haveDigit = True
-            elif not haveSpecChar and char in SpecChar: haveSpecChar = True
+            if not haveUpper and char.isupper(): haveUpper = True
+            if not haveLower and char.islower(): haveLower = True
+            if not haveDigit and char.isnumeric(): haveDigit = True
+            if not haveSpecChar and char in SpecChar: haveSpecChar = True
             last_char = char
         return haveUpper and haveLower and haveDigit and haveSpecChar
