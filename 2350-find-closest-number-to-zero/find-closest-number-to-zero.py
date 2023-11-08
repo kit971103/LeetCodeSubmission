e970a80b5fd78ab7ae0c2359135b8b3de44f5bcf
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-08 14:39:54 -0800

    Time: 140 ms (51.17%) | Memory: 16.5 MB (68.80%) - LeetSync

--- 2350-find-closest-number-to-zero/find-closest-number-to-zero.py
+++ 2350-find-closest-number-to-zero/find-closest-number-to-zero.py
@@ -1,4 +1,4 @@
 class Solution:
     def findClosestNumber(self, nums: List[int]) -> int:
-        return min(nums, key = lambda n: abs(n) - (0.5 if n > 0 else 0))
+        return min(nums, key = lambda n: (abs(n), -n))
         
