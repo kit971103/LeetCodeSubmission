9150b4c0d159f02eb45bc860b3a7f6d035315982
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-08 00:03:55 -0800

    Time: 38 ms (51.60%) | Memory: 16.3 MB (28.28%) - LeetSync

--- 2955-account-balance-after-rounded-purchase/account-balance-after-rounded-purchase.py
+++ 2955-account-balance-after-rounded-purchase/account-balance-after-rounded-purchase.py
@@ -1,3 +1,3 @@
 class Solution:
     def accountBalanceAfterPurchase(self, purchaseAmount: int) -> int:
-        return 100 - (purchaseAmount//10 + (1 if purchaseAmount%10 > 4 else 0))*10
+        return 100 - purchaseAmount//10 * 10 - (10 if purchaseAmount%10 > 4 else 0)
