75539ca808c292bcd64c8b75865ac0d51258ca48
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-09 15:27:53 -0800

    Time: 45 ms (9.22%) | Memory: 16.1 MB (89.88%) - LeetSync

--- 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.py
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.py
@@ -1,9 +1,5 @@
 class Solution:
     def numberOfMatches(self, n: int) -> int:
-        match = 0
-        while n > 1:
-            match += n//2
-            n = math.ceil(n/2)
-        return match
+        return n-1
         
         
