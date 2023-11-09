41b02a88ef8353ed86632d2393a2e2a33ed238e2
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-09 13:08:17 -0800

    Time: 103 ms (80.28%) | Memory: 17.2 MB (77.82%) - LeetSync

--- 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.py
+++ 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.py
@@ -1,21 +1,14 @@
 class Solution:
     def countHomogenous(self, s: str) -> int:
-        memorization = dict()
         length = 1
         res = 0
         for a, b in itertools.pairwise(s):
             if a != b:
-                if length not in memorization: 
-                    memorization[length] = (length+1)*length//2
-                res += memorization[length]
+                res += (length+1)*length//2
                 length = 1
             else: 
                 length += 1
-        
-        if length not in memorization: 
-            memorization[length] = (length+1)*length//2
-        res += memorization[length]
-
+        res += (length+1)*length//2
         return res%(10**9+7)
         
         
