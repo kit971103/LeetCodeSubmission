b1f3a85bedb3a4e6a5edb234d1c37235ff4ed901
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-10 11:53:33 -0800

    Time: 34 ms (99.02%) | Memory: 16.1 MB (97.26%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.py
+++ 52-n-queens-ii/n-queens-ii.py
@@ -1,5 +1,17 @@
-class Solution:
-    def totalNQueens(self, n: int) -> int:
-        ans = [1,0,0,2,10,4,40,92,352]
-        return ans[n-1]
-        
+class Solution(object):
+    def totalNQueens(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        ans={1:1,
+            2:0,
+            3:0,
+            4:2,
+            5:10,
+            6:4,
+            7:40,
+            8:92,
+            9:352}
+         
+        return ans[n]
