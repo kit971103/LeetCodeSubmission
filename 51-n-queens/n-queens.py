e019a3cda0d970fdeb731a49506ee0d6934b2d00
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-10 11:44:38 -0800

    Time: 66 ms (44.31%) | Memory: 16.7 MB (52.85%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -2,34 +2,20 @@ class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         def dfs(k):
             if k >= n:
-                rep = [ "".join("Q" if j == Q_position else "." for j in range(n)) for Q_position in sol]
-                # rep= []
-                # for each_Q_position in sol:
-                #     row = ["Q" if j == each_Q_position else "." for j in range(n)]
-                #     row = "".join(row)
-                #     rep.append(row)
-                # print(f"Found one solution:{sol}, {rep=}")
-                res.append(rep)
+                res.append([ "".join("Q" if j == Q_position else "." for j in range(n)) for Q_position in sol])
                 return
-            
 
-            possible_position = [True] * n
+            possible_position = set(range(n))
             for row, Q_position in enumerate(sol):
-                possible_position[Q_position] = False
                 d = k - row
-                left_down = Q_position - d
-                right_down = Q_position + d
-                if left_down >= 0:  possible_position[left_down] = False
-                if right_down < n: possible_position[right_down] = False
-            possible_position = [index for index, val in enumerate(possible_position) if val]
-            
-            # print(f"At row {k}, {sol=}, {possible_position=}")
+                possible_position.discard(Q_position)
+                possible_position.discard(Q_position+d)
+                possible_position.discard(Q_position-d)
+
             for Q_position in possible_position:
-                # print(f"\tTry {Q_position= }")
                 sol.append(Q_position)
                 dfs(k+1)
                 sol.pop()
-                # print(f"\tDone {Q_position= }")
 
         res = []
         sol = []
