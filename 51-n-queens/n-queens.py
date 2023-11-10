a844eb1ab9b3eae66877aace594a300512bff0d1
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-10 11:42:08 -0800

    Time: 70 ms (40.82%) | Memory: 16.9 MB (22.32%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,18 +1,41 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        
-        res = []
-        for solution in itertools.permutations(range(n)):
-            for row, col in enumerate(solution):
-                for i in range(row + 1, n):
-                    j = solution[i]
-                    d = i - row
-                    if (j == col + d) or (j == col - d): break
-                else: continue
-                break
-            else: res.append(["".join( "Q" if j == i else "." for j in range(n) ) for i in solution])
-        return res
+        def dfs(k):
+            if k >= n:
+                rep = [ "".join("Q" if j == Q_position else "." for j in range(n)) for Q_position in sol]
+                # rep= []
+                # for each_Q_position in sol:
+                #     row = ["Q" if j == each_Q_position else "." for j in range(n)]
+                #     row = "".join(row)
+                #     rep.append(row)
+                # print(f"Found one solution:{sol}, {rep=}")
+                res.append(rep)
+                return
+            
 
+            possible_position = [True] * n
+            for row, Q_position in enumerate(sol):
+                possible_position[Q_position] = False
+                d = k - row
+                left_down = Q_position - d
+                right_down = Q_position + d
+                if left_down >= 0:  possible_position[left_down] = False
+                if right_down < n: possible_position[right_down] = False
+            possible_position = [index for index, val in enumerate(possible_position) if val]
+            
+            # print(f"At row {k}, {sol=}, {possible_position=}")
+            for Q_position in possible_position:
+                # print(f"\tTry {Q_position= }")
+                sol.append(Q_position)
+                dfs(k+1)
+                sol.pop()
+                # print(f"\tDone {Q_position= }")
+
+        res = []
+        sol = []
+        dfs(0)
+        return res
+        
 
             
 
