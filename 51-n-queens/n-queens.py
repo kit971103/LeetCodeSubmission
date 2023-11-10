9e98b9bec5acb76d52b36713bd921eba2c409cbd
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-10 00:05:58 -0800

    Time: 347 ms (5.98%) | Memory: 16.7 MB (53.10%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -3,17 +3,14 @@ class Solution:
         
         res = []
         for solution in itertools.permutations(range(n)):
-            OK = True
             for row, col in enumerate(solution):
                 for i in range(row + 1, n):
                     j = solution[i]
                     d = i - row
-                    if (j == col + d) or (j == col - d): 
-                        OK = False
-                        break
-                if not OK: break
-            else:
-                res.append(["".join( "Q" if j == i else "." for j in range(n) ) for i in solution])
+                    if (j == col + d) or (j == col - d): break
+                else: continue
+                break
+            else: res.append(["".join( "Q" if j == i else "." for j in range(n) ) for i in solution])
         return res
 
 
