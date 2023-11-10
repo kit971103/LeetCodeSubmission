1646a7af84e903aad7847fbea148557beb87ba1b
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-10 15:39:43 -0800

    Time: 22 ms (99.90%) | Memory: 16.3 MB (17.46%) - LeetSync

--- 796-reaching-points/reaching-points.py
+++ 796-reaching-points/reaching-points.py
@@ -1,8 +1,5 @@
 class Solution:
     def reachingPoints(self, sx: int, sy: int, tx: int, ty: int) -> bool:
-        print(f"init: {sx=}, {sy=}")
-        print(f"\t{tx=}, {ty=}")
-
         while ty >= sy and tx >= sx:
             if tx == sx:
                 return (ty - sy)%sx == 0
@@ -10,6 +7,4 @@ class Solution:
                 return (tx - sx)%sy == 0
             elif ty > tx: ty %= tx
             else: tx %= ty
-            print(f"\t{tx=}, {ty=}")
-        
         return ty == sy and tx == sx
