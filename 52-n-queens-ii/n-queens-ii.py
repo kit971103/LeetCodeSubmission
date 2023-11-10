9d73236c6abeddfb06ab512373f447ee5aae1376
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-10 11:55:30 -0800

    Time: 42 ms (91.71%) | Memory: 16.3 MB (27.21%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.py
+++ 52-n-queens-ii/n-queens-ii.py
@@ -1,17 +1,23 @@
-class Solution(object):
-    def totalNQueens(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        ans={1:1,
-            2:0,
-            3:0,
-            4:2,
-            5:10,
-            6:4,
-            7:40,
-            8:92,
-            9:352}
-         
-        return ans[n]
+class Solution:
+    def totalNQueens(self, n: int) -> List[List[str]]:
+        def dfs(k):
+            if k >= n:
+                nonlocal count
+                count += 1
+                return
+
+            possible_position = set(range(n))
+            for row, Q_position in enumerate(sol):
+                d = k - row
+                possible_position.discard(Q_position)
+                possible_position.discard(Q_position+d)
+                possible_position.discard(Q_position-d)
+
+            for Q_position in possible_position:
+                sol.append(Q_position)
+                dfs(k+1)
+                sol.pop()
+        sol =[]
+        count = 0
+        dfs(0)
+        return count
