621db964e2c041a5de47c9a3caeedb042769d63b
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-11 23:24:37 -0800

    Time: 437 ms (97.38%) | Memory: 41.1 MB (65.25%) - LeetSync

--- 833-bus-routes/bus-routes.py
+++ 833-bus-routes/bus-routes.py
@@ -1,42 +1,42 @@
 class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
-        
         if source == target: return 0
-
-        routes_adj_list = []
         
-        for i in range(len(routes)):
-            routes[i] = set(routes[i])
-            routes_adj_list.append(set())
-            for stop in routes[i]:
-                if len(routes_adj_list[i]) >= i: break
-                for j in range(i):
-                    if stop in routes[j]: 
-                        routes_adj_list[i].add(j)
-                        routes_adj_list[j].add(i)
-
+        bus_in_stop = defaultdict(list)
+        
+        for bus, route in enumerate(routes):
+            for stop in route:
+                bus_in_stop[stop].append(bus)
+        
+        queue = bus_in_stop[source]
         length = 1
-        source = set( i for i, route in enumerate(routes) if source in route)
-        queue = []
-        for i, route in enumerate(routes):
-            if target not in route: continue
-            if i in source: return length
-            queue.append(i)
-
-        seen = set(queue)
+        visited_stop = {source}
+        visited_bus = set(queue)
 
         while queue:
-            new_queue = []
-            length += 1
-            for route in queue:
-                for neighbour in routes_adj_list[route]:
-                    if neighbour not in seen: 
-                        if neighbour in source: return length
-                        new_queue.append(neighbour)
-                        seen.add(neighbour)
-            queue = new_queue.copy()
+            next_queue = []
+            for bus in queue:
+                for stop in routes[bus]:
+                    if stop in visited_stop: continue
+                    if stop == target: return length
+                    visited_stop.add(stop)
+                    for next_bus in bus_in_stop[stop]:
+                        if next_bus not in visited_bus:
+                            visited_bus.add(next_bus)
+                            next_queue.append(next_bus)
+            queue = next_queue.copy()
+            length+=1
         return -1
+                    
+                    
 
+
+
+
+
+        
+
+            
 
 
 
