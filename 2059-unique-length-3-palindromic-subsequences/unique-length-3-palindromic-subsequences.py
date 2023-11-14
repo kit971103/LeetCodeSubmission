c396f39de2f4f59fa717d1744d9e8837cb86c7d8
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-14 10:36:58 -0800

    Time: 1754 ms (40.69%) | Memory: 17.3 MB (43.97%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -9,13 +9,18 @@ class Solution:
         not_active_range.sort(reverse = True, key = lambda x: x[1])
         active_range = []
         res = set()
+        old = None
 
         for i, c in enumerate(s):
+            updated = False
             if not_active_range and i > not_active_range[-1][1]:
                 heapq.heappush(active_range, not_active_range.pop())
+                updated = True
             if active_range and i >= active_range[0][0]:
                 heapq.heappop(active_range)
-            res.update( x+c+x for _, __, x in active_range )
+                updated = True
+            if updated or old!= c: res.update( x+c+x for _, __, x in active_range )
+            old = c
         return len(res)
 
 
