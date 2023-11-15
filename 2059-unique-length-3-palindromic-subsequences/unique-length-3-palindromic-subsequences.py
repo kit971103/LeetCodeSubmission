aba6462ab1d5281650fe12c3ab06e6254f7c1baa
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-14 17:45:18 -0800

    Time: 117 ms (99.45%) | Memory: 22 MB (5.14%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -1,27 +1,18 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        seen = dict()
-        for i, c in enumerate(s):
-            if c not in seen: seen[c] = [i,i]
-            else: seen[c][1] = i
+        char_location = defaultdict(list)
+        for i, c in enumerate(s): 
+            char_location[c].append(i)
+
+        res = 0
+        for p_c, p_location in char_location.items():
+            if len(p_location) < 2: continue
+            for mid_c, mid_location in char_location.items():
+                if bisect_left(mid_location, p_location[0]) < bisect_left(mid_location, p_location[-1]):
+                    res+=1
+            if len(p_location) == 2: res -= 1
+        return res
+
         
-        not_active_range = [ (end, start, c) for c, (start, end) in seen.items() if start != end]
-        not_active_range.sort(reverse = True, key = lambda x: x[1])
-        active_range = []
-        res = set()
-        old = None
-
-        for i, c in enumerate(s):
-            updated = False
-            if not_active_range and i > not_active_range[-1][1]:
-                heapq.heappush(active_range, not_active_range.pop())
-                updated = True
-            if active_range and i >= active_range[0][0]:
-                heapq.heappop(active_range)
-                updated = True
-            if updated or old!= c: res.update( x+c+x for _, __, x in active_range )
-            old = c
-        return len(res)
-
 
         
