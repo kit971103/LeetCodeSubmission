5785c2fe3ac4f785a382dcebf2cf51621d42616a
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-14 18:38:09 -0800

    Time: 3841 ms (10.32%) | Memory: 36.3 MB (21.25%) - LeetSync

--- 2953-count-pairs-of-points-with-distance-k/count-pairs-of-points-with-distance-k.py
+++ 2953-count-pairs-of-points-with-distance-k/count-pairs-of-points-with-distance-k.py
@@ -1,16 +1,14 @@
 class Solution:
     def countPairs(self, coordinates: List[List[int]], k: int) -> int:
-        
-        c=0
-        hasmap = collections.Counter()
-        
-       
+        #
+        # this is just a 2D 2-sum problem, slightly different condition
+        #
+        count = Counter()
+        res = 0
         for x1, y1 in coordinates:
-            
-            for x in range(k+1):
-                c += hasmap[ x1^x, y1^(k-x) ]
-            
-            hasmap[x1, y1]+=1   
-        
-        return c
-        
+            for x in range(k + 1):
+                # so we have x1^x2 = x and y1^y2=(k-x)
+                # then x1^x2+y1^y2 = k
+                res += count[x1 ^ x, y1 ^ (k - x)]
+            count[x1, y1] += 1
+        return res
