cf163beec92aa68137eed290d5507f332f150185
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-16 01:03:55 -0800

    Time: 341 ms (59.59%) | Memory: 31.4 MB (7.76%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.py
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.py
@@ -1,7 +1,7 @@
 class Solution:
     def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
-        acc_arr = [ n for n in itertools.accumulate(arr, func = operator.xor)]
-        return [ (acc_arr[left-1] if left else 0)^acc_arr[right] for left, right in queries]
+        acc_arr = [ n for n in itertools.accumulate(arr, func = operator.xor, initial = 0)]
+        return [ acc_arr[left]^acc_arr[right+1] for left, right in queries]
 
 
         
