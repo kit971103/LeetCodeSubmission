4a3c43bf38b505759b1046ed02ddaa7a27e236ca
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-16 12:48:53 -0800

    Time: 231 ms (49.03%) | Memory: 16.4 MB (72.92%) - LeetSync

--- 688-knight-probability-in-chessboard/knight-probability-in-chessboard.py
+++ 688-knight-probability-in-chessboard/knight-probability-in-chessboard.py
@@ -1,20 +1,22 @@
 class Solution:
     def knightProbability(self, n: int, k: int, row: int, column: int) -> float:
         
-        layer = collections.Counter()
-        layer[(row, column)] += 1
         steps = ((+1, +2), (+2, +1), (+1, -2), (+2, -1), (-1, +2), (-2, +1), (-1, -2), (-2, -1))
+        def addPossibleMove(y, x):
+            for dy, dx in steps:
+                new_y = y + dy
+                new_x = x + dx
+                if 0 <= new_y < n and 0 <= new_x < n:
+                    nextlayer[new_y, new_x] += layer[y,x]
+
+
+        layer = collections.Counter()
+        layer[row, column] = 1
 
         for _ in range(k):
-            
-            nextlayer = collections.Counter()
-            for y, x in layer.keys():
-                for dy, dx in steps:
-                    ny = y+dy
-                    nx = x+dx
-                    if 0 <= ny < n and 0 <= nx < n: 
-                        nextlayer[(ny, nx)]+=layer[(y,x)]
-            
+            nextlayer = collections.Counter() 
+            for point in layer:
+                addPossibleMove(*point)
             layer = nextlayer.copy()
 
         return layer.total()/8**k
