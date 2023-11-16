3e9af91fb96602b641b7043a966494326cf54be1
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-16 12:27:01 -0800

    Time: 325 ms (13.88%) | Memory: 16.5 MB (67.01%) - LeetSync

--- 688-knight-probability-in-chessboard/knight-probability-in-chessboard.py
+++ 688-knight-probability-in-chessboard/knight-probability-in-chessboard.py
@@ -5,14 +5,7 @@ class Solution:
         steps = ((+1, +2), (+2, +1), (+1, -2), (+2, -1), (-1, +2), (-2, +1), (-1, -2), (-2, -1))
 
         for _ in range(k):
-            # print(f"depth: {_}")
-            # for _ in dp:
-            #     print(f"\t{_}")
-
-            temp = [[0] * n for _ in range(n)]
-            for y, x in itertools.product(range(n), repeat = 2):
-                temp[y][x] = sum( dp[y+dy][x+dx] for dy, dx in steps if (0 <= y+dy < n)and(0 <= x+dx < n))
-            
+            temp = [ [ sum( dp[y+dy][x+dx] for dy, dx in steps if (0 <= y+dy < n)and(0 <= x+dx < n)) for x in range(n)] for y in range(n)]
             dp = copy.deepcopy(temp)
         
         return dp[row][column]/8**k
