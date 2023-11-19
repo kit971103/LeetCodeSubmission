7e4a398a23acf4822150ebc0d6a3a1a85e9b0aff
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-18 16:46:44 -0800

    Time: 897 ms (23.71%) | Memory: 23.9 MB (45.88%) - LeetSync

--- 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.py
+++ 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.py
@@ -1,14 +1,16 @@
 class Solution:
     def reductionOperations(self, nums: List[int]) -> int:
-        nums.sort(reverse = True)
-        smallest  = nums[-1]
-        res = level = 0
-        for i in range(len(nums)-1):
-            if nums[i] == smallest: return res
-            level += 1
-            if nums[i] != nums[i+1]:
-                res+=level
+        nums.sort()
+        smallest_i = bisect.bisect_right(nums, nums[0])-1
+        res = 0
+        n = len(nums)
+        i = n-1
+        while i > smallest_i:
+            i = bisect.bisect_left(nums, nums[i])-1
+            res += n-i-1
         return res
+
+
 
 
 
