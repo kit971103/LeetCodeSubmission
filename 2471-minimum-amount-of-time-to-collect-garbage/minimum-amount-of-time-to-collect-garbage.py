3439cc77df0150dbdbe0286ed1d3de77813d1272
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-19 23:58:35 -0800

    Time: 797 ms (92.23%) | Memory: 39.2 MB (83.14%) - LeetSync

--- 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.py
+++ 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.py
@@ -1,13 +1,20 @@
 class Solution:
     def garbageCollection(self, garbage: List[str], travel: List[int]) -> int:
         last_seen_P = last_seen_G = last_seen_M = res = 0
-        total_travled = list(n for n in itertools.accumulate(travel, initial = 0))
         for i, recycle_box in enumerate(garbage):
             res += len(recycle_box)
             if "P" in recycle_box: last_seen_P = i
             if "G" in recycle_box: last_seen_G = i
             if "M" in recycle_box: last_seen_M = i
-        return res + total_travled[last_seen_P] + total_travled[last_seen_G] + total_travled[last_seen_M]
+        acc = 0
+        for i, time in enumerate(travel, 1):
+            acc+=time
+            if i == last_seen_P: res += acc
+            if i == last_seen_G: res += acc
+            if i == last_seen_M: res += acc
+            if i >= last_seen_P and i >= last_seen_G and i >= last_seen_M: break
+            
+        return res 
             
             
 
