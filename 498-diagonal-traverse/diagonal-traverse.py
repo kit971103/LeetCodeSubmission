456cade19f89489a07e663b4d331e2c7e17ab7f0
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-21 22:35:10 -0800

    Time: 191 ms (10.87%) | Memory: 20.1 MB (49.57%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.py
+++ 498-diagonal-traverse/diagonal-traverse.py
@@ -1,37 +1,15 @@
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
-        rows, cols = len(mat), len(mat[0])
-        row, col = 0, 0
-        direction = "RU"
         res = []
-        for _ in range(rows*cols):
-            
-            res.append(mat[row][col])
-            
-            if direction == "RU":
-                hit_wall = (row == 0 or col == cols-1)
-                if not hit_wall:
-                    row -= 1
-                    col += 1 
-                else:
-                    direction = "LD"
-                    if col+1 < cols:
-                        col +=1
-                    else:
-                        row += 1
-            
+        rows, cols = len(mat), len(mat[0])
+        for total in range(rows + cols - 1):
+            if total%2:
+                res.extend(mat[row][total - row] for row in range(max(total - cols + 1, 0), min(total+1, rows)))
             else:
-                hit_wall = (row == rows-1 or col == 0)
-                if not hit_wall:
-                    row += 1
-                    col -= 1
-                else:
-                    direction = "RU"
-                    if row+1 < rows:
-                        row +=1
-                    else:
-                        col += 1
+                res.extend(mat[total - col][col] for col in range(max(total - rows + 1, 0), min(total+1, cols)))
         return res
+
+
 
 
 
