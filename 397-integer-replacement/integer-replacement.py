ebd2d625e3e16381560b95761e935904006454a2
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-23 14:14:35 -0800

    Time: 149 ms (27.73%) | Memory: 16.4 MB (9.51%) - LeetSync

---
+++ 397-integer-replacement/integer-replacement.py
@@ -0,0 +1,8 @@
+class Solution:
+    def integerReplacement(self, n: int) -> int:
+        if n == 1: return 0
+        if n%2:
+            return min(self.integerReplacement(n-1), self.integerReplacement(n+1)) + 1
+        else:
+            return self.integerReplacement(n//2)+1
+        
