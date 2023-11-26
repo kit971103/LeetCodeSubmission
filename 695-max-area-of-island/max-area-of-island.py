ae683aafc99fb8fb5f449ffd1f857650125293a1
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-25 17:24:54 -0800

    Time: 124 ms (70.00%) | Memory: 19.1 MB (56.90%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -3,19 +3,19 @@ class Solution:
         rows, cols = len(grid), len(grid[0])
 
         def bfs(row, col):
-            if  not( 0 <= row < rows and 0 <= col < cols) or visited[row][col]: 
+            
+            if  not( 0 <= row < rows and 0 <= col < cols) or grid[row][col] == 0: 
                 return
-            visited[row][col] = True
-            if grid[row][col]:
-                nonlocal area
-                area += 1
-                bfs(row+1, col)
-                bfs(row-1, col)
-                bfs(row, col+1)
-                bfs(row, col-1)
+            
+            nonlocal area
+            area += 1
+            grid[row][col] = 0
+            bfs(row+1, col)
+            bfs(row-1, col)
+            bfs(row, col+1)
+            bfs(row, col-1)
         
         max_area = 0
-        visited = [ [False] * cols for _ in range(rows)]
         for row, col in itertools.product(range(rows), range(cols)):
             area = 0
             bfs(row, col)
