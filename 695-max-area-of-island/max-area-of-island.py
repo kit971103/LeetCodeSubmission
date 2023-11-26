94d9e40307cc4d2c8f7e3d0de6c7c6809d2f571b
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-25 17:22:19 -0800

    Time: 119 ms (82.43%) | Memory: 19.1 MB (59.42%) - LeetSync

---
+++ 695-max-area-of-island/max-area-of-island.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+
+        def bfs(row, col):
+            if  not( 0 <= row < rows and 0 <= col < cols) or visited[row][col]: 
+                return
+            visited[row][col] = True
+            if grid[row][col]:
+                nonlocal area
+                area += 1
+                bfs(row+1, col)
+                bfs(row-1, col)
+                bfs(row, col+1)
+                bfs(row, col-1)
+        
+        max_area = 0
+        visited = [ [False] * cols for _ in range(rows)]
+        for row, col in itertools.product(range(rows), range(cols)):
+            if visited[row][col]: 
+                continue
+            area = 0
+            bfs(row, col)
+            if area > max_area: max_area = area
+        
+        return max_area
+
+
+
+            
+        
