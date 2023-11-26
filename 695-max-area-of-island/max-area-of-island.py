19edd2b8fbebc2831141f06d971fc5c68cf4eaa3
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-25 17:25:59 -0800

    Time: 114 ms (91.82%) | Memory: 19 MB (63.97%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -3,23 +3,21 @@ class Solution:
         rows, cols = len(grid), len(grid[0])
 
         def bfs(row, col):
-            
-            if  not( 0 <= row < rows and 0 <= col < cols) or grid[row][col] == 0: 
-                return
-            
-            nonlocal area
-            area += 1
-            grid[row][col] = 0
-            bfs(row+1, col)
-            bfs(row-1, col)
-            bfs(row, col+1)
-            bfs(row, col-1)
+            if  0 <= row < rows and 0 <= col < cols and grid[row][col]: 
+                nonlocal area
+                area += 1
+                grid[row][col] = 0
+                bfs(row+1, col)
+                bfs(row-1, col)
+                bfs(row, col+1)
+                bfs(row, col-1)
         
         max_area = 0
         for row, col in itertools.product(range(rows), range(cols)):
-            area = 0
-            bfs(row, col)
-            if area > max_area: max_area = area
+            if grid[row][col]:
+                area = 0
+                bfs(row, col)
+                if area > max_area: max_area = area
         
         return max_area
 
