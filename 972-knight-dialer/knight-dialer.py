80f1bc5d1f0f8d61e8a51f0017004cb1bca13d99
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-27 00:52:39 -0800

    Time: 1227 ms (56.41%) | Memory: 16.5 MB (73.06%) - LeetSync

--- 972-knight-dialer/knight-dialer.py
+++ 972-knight-dialer/knight-dialer.py
@@ -1,22 +1,12 @@
 class Solution:
     def knightDialer(self, n: int) -> int:
-        # posible_move = [(4,6), (6,8), (7,9), (4,8), (0, 3,9),  tuple(), (0, 1,7), (2,6), (1,3), (2,4), (4,6) ]
+        posible_move = [(4,6), (6,8), (7,9), (4,8), (0,3,9),  tuple(), (0,1,7), (2,6), (1,3), (2,4), (4,6)]
 
         posible_numbers_end_with = [1] * 10
-        if n != 1: posible_numbers_end_with[5] = 0
         mod = 10**9+7
         for _ in range(n-1):
-            temp_store = posible_numbers_end_with.copy()
-
-            posible_numbers_end_with[0] = (temp_store[4] + temp_store[6])%mod
-            posible_numbers_end_with[1] = (temp_store[6] + temp_store[8])%mod
-            posible_numbers_end_with[2] = (temp_store[7] + temp_store[9])%mod
-            posible_numbers_end_with[3] = (temp_store[4] + temp_store[8])%mod
-            posible_numbers_end_with[4] = (temp_store[0] + temp_store[3] + temp_store[9])%mod
-            # posible_numbers_end_with[5] = 0
-            posible_numbers_end_with[6] = (temp_store[0] + temp_store[1] + temp_store[7])%mod
-            posible_numbers_end_with[7] = (temp_store[2] + temp_store[6])%mod
-            posible_numbers_end_with[8] = (temp_store[1] + temp_store[3])%mod
-            posible_numbers_end_with[9] = (temp_store[2] + temp_store[4])%mod
+            last_iter = posible_numbers_end_with.copy()
+            for i in range(10):
+                posible_numbers_end_with[i] = sum(last_iter[j] for j in posible_move[i])
 
         return sum(posible_numbers_end_with)%mod
