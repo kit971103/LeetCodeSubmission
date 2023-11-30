f9903a59c9d2038dad70b9053ef39d3cde918f83
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-11-29 22:44:56 -0800

    Time: 34 ms (89.61%) | Memory: 16.3 MB (27.27%) - LeetSync

--- 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.py
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.py
@@ -1,17 +1,9 @@
 class Solution:
     def minimumOneBitOperations(self, n: int) -> int:
-        try:
-            length = math.floor(math.log(n, 2))+1
-        except:
-            return 0
-        res = count = 0
-        for i in range(length, -1, -1):
-            if n & (1 << i):
-                count += 1
-                if count%2:
-                    res += 2**(i+1)-1
-                else:
-                    res -= 2**(i+1)-1
+        res = 0
+        while n:
+            res ^= n
+            n >>= 1
         return res
             
         
