7280d4dd4c80149246b6cf450cfabbbb843cdaac
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-12-02 18:57:59 -0800

    Time: 51 ms (98.67%) | Memory: 16.5 MB (30.54%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.py
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.py
@@ -1,8 +1,5 @@
 class Solution:
     def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
-        res = 0
-        for i in range(1, len(points)):
-            res += max(abs(points[i][0]-points[i-1][0]), abs(points[i][1]-points[i-1][1]))
-        return res
+        return sum([max(abs(points[i][0]-points[i-1][0]), abs(points[i][1]-points[i-1][1])) for i in range(1, len(points)) ])
 
         
