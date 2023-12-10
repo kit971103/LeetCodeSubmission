5705422096023f7930cfd719551a9dd9855cdf49
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-12-10 13:14:23 -0800

    Time: 65 ms (88.87%) | Memory: 17.2 MB (25.95%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.py
+++ 871-keys-and-rooms/keys-and-rooms.py
@@ -2,9 +2,10 @@ class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
         def dfs(keys):
             for key in keys:
-                if not visited[key]:
-                    visited[key] = True
-                    dfs(rooms[key])
+                if visited[key]: continue
+                visited[key] = True
+                dfs(rooms[key])
+        
         visited = [False] * len(rooms)
         visited[0] = True
         dfs(rooms[0])
