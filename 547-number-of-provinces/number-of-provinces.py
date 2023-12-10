dd3f9339ccb8c5c28ba03c9aecd4792b7c893909
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-12-10 13:58:16 -0800

    Time: 199 ms (89.99%) | Memory: 17.7 MB (7.06%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,20 +1,20 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        def visit(city):
+            if visited[city]:
+                return
+            visited[city] = True
+            for connectint_city, connected in enumerate(isConnected[city]):
+                if connected:
+                    visit(connectint_city)
+
         count = 0
         visited = [False] * len(isConnected)
         for city in range(len(isConnected)):
             if visited[city]:
                 continue
-            count +=1
-            stack = deque([city])
-            while stack:
-                for _ in range(len(stack)):
-                    city = stack.popleft()
-                    if visited[city]:
-                        continue
-                    visited[city] = True
-                    for i, x in enumerate(isConnected[city]):
-                        if x: stack.append(i)
+            count+=1
+            visit(city)
         return count
 
 
