c49710a6633d58710f97a318b9fbb8b9762bc3ba
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-12-12 22:15:43 -0800

    Time: 143 ms (95.72%) | Memory: 16.7 MB (33.33%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.py
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.py
@@ -1,13 +1,12 @@
 class Solution:
     def numSpecial(self, mat: List[List[int]]) -> int:
         count = 0
-        for row, col in product(range(len(mat)), range(len(mat[0]))):
-            # print(f"{row}, {col}")
-            # print(mat[row])
-            # print([mat[i][col] for i in range(len(mat))])
-            if sum(mat[row]) == 1 and sum([mat[i][col] for i in range(len(mat))])==1 and mat[row][col] == 1:
-                # print(f"add at {row}, {col}")
-                count +=1
+        for row in mat:
+            if sum(row) != 1:
+                continue
+            col = row.index(1)
+            if sum( mat[i][col] for i in range(len(mat))) == 1:
+                count += 1
         return count
 
         
