db33865d144eb0a6c67f95431c8196a3cd90cf51
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-12-14 12:50:42 -0800

    Time: 69 ms (7.23%) | Memory: 16.2 MB (75.82%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,37 +1,37 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        time, count = 0, 1
+        time, count = -1, 1
         rows, cols =len(grid), len(grid[0])
-        checked = [ [False]*cols for _ in range(rows)]
+        checked = set()
 
         while count:
             count = 0
             time += 1
-            visited = [ [False]*cols for _ in range(rows)]
+            visited = set()
             for row, col in product(range(rows), range(cols)):
-                if not checked[row][col] and not visited[row][col] and grid[row][col] == 2:
-                    checked[row][col] = True
+                if not( (row,col) in checked or (row, col) in visited) and grid[row][col] == 2:
+                    checked.add( (row,col) )
                     if 0 < row and grid[row-1][col] == 1:
                         count += 1
                         grid[row-1][col] = 2
-                        visited[row-1][col] = True
+                        visited.add( (row-1,col) )
                     if row+1 < rows and grid[row+1][col] == 1:
                         count += 1
                         grid[row+1][col] = 2
-                        visited[row+1][col] = True
+                        visited.add( (row+1,col) )
                     if 0 < col and grid[row][col-1] == 1:
                         count += 1
                         grid[row][col-1] = 2
-                        visited[row][col-1] = True
+                        visited.add( (row,col-1) )
                     if col+1 < cols and grid[row][col+1] == 1:
                         count += 1
                         grid[row][col+1] = 2
-                        visited[row][col+1] = True
+                        visited.add( (row,col+1) )
 
         for row, col in product(range(rows), range(cols)):
             if grid[row][col] == 1:
                 return -1
-        return time-1
+        return time
         
 
         
