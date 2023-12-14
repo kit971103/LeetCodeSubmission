17f61d21bba00715b96caf4f6e7cd86c57346fc7
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-12-14 12:50:58 -0800

    Time: 66 ms (11.83%) | Memory: 16.3 MB (75.82%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,37 +1,31 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        time, count = -1, 1
+        time, count = 0, 1
         rows, cols =len(grid), len(grid[0])
-        checked = set()
 
-        while count:
+        while count!=0:
             count = 0
             time += 1
-            visited = set()
+            visited = [ [False]*cols for _ in range(rows)]
             for row, col in product(range(rows), range(cols)):
-                if not( (row,col) in checked or (row, col) in visited) and grid[row][col] == 2:
-                    checked.add( (row,col) )
-                    if 0 < row and grid[row-1][col] == 1:
-                        count += 1
-                        grid[row-1][col] = 2
-                        visited.add( (row-1,col) )
-                    if row+1 < rows and grid[row+1][col] == 1:
-                        count += 1
-                        grid[row+1][col] = 2
-                        visited.add( (row+1,col) )
-                    if 0 < col and grid[row][col-1] == 1:
-                        count += 1
-                        grid[row][col-1] = 2
-                        visited.add( (row,col-1) )
-                    if col+1 < cols and grid[row][col+1] == 1:
-                        count += 1
-                        grid[row][col+1] = 2
-                        visited.add( (row,col+1) )
+                if visited[row][col]:
+                    continue
+                if grid[row][col] == 2:
+                    for dy, dx in ((+1, 0), (-1,0), (0, +1), (0, -1)):
+                        if 0 <= row+dy <rows and 0 <= col+dx < cols and grid[row+dy][col+dx] == 1:
+                            count += 1
+                            grid[row+dy][col+dx] = 2
+                            visited[row+dy][col+dx] = True
+                else:
+                    visited[row][col] = True
 
         for row, col in product(range(rows), range(cols)):
             if grid[row][col] == 1:
                 return -1
-        return time
+        return time-1
+        
+
+        
         
 
         
