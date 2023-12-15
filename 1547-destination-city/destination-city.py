1ab70342d812b58e466b0a209fbd4d4aae02d8e1
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-12-14 17:32:34 -0800

    Time: 64 ms (43.94%) | Memory: 16.4 MB (45.28%) - LeetSync

--- 1547-destination-city/destination-city.py
+++ 1547-destination-city/destination-city.py
@@ -1,10 +1,18 @@
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
-        adj = Counter()
+        seen_start = set()
+        seen_end = set()
         for start, end in paths:
-            adj[start] -= 1
-            adj[end] += 1
-        return adj.most_common(1)[0][0]
+            if start in seen_end:
+                seen_end.remove(start)
+            else:
+                seen_start.add(start)
+            
+            if end in seen_start:
+                seen_start.remove(end)
+            else:
+                seen_end.add(end)    
+        return seen_end.pop()
 
 
             
