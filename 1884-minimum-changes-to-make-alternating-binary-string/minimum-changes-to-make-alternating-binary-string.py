41165c36dff60da28b0adaabb4061dcb4448fd61
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-12-23 18:05:32 -0800

    Time: 44 ms (88.66%) | Memory: 17.4 MB (5.67%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
@@ -2,8 +2,7 @@ class Solution:
     def minOperations(self, s: str) -> int:
         p=0
         for i, c in enumerate(s):
-            if (i%2)^(c=="1"):
-                p+=1
+            p+=(i%2)^(c=="1")
         
         return min(p,len(s)-p)
         
