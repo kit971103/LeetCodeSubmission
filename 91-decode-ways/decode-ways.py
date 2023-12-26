526ef1f6471877ae7baae5484f75bf54bdd07f00
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-12-25 17:11:32 -0800

    Time: 32 ms (91.86%) | Memory: 17.3 MB (5.04%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,6 +1,4 @@
 class Solution:
-    one_two = ("1", "2")
-    one_six = ("1","2","3","4","5","6")
     def numDecodings(self, s: str) -> int:
 
         if s[0] == "0":
@@ -8,15 +6,15 @@ class Solution:
         
         last = s[0]
         bound = 1
-        unbound = 1 if last in Solution.one_two else 0
+        unbound = 1 if int(last) < 3 else 0
 
         for cur in islice(s,1,None):
             if cur == "0":
-                if last not in Solution.one_two:
+                if int(last)>2:
                     return 0
                 bound = unbound
                 unbound = 0
-            elif last == "1" or (last =="2" and cur in Solution.one_six):
+            elif last == "1" or (last =="2" and int(cur) <7):
                 bound += unbound
                 unbound = bound - unbound
             else:
