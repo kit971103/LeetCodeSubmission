9f8f046b70b892d39455f37fd132f2a0eb2a4dc4
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-12-25 18:39:14 -0800

    Time: 847 ms (91.53%) | Memory: 17.5 MB (44.07%) - LeetSync

--- 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.py
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.py
@@ -1,6 +1,6 @@
 class Solution:
     def lenLongestFibSubseq(self, arr: List[int]) -> int:
-        res = 0
+        res = 2
         quick_check = set(arr)
         for i in range(len(arr)):
             for ii in range(i+1, len(arr)):
@@ -10,7 +10,10 @@ class Solution:
                 while (x0+x1) in quick_check:
                     x0, x1 = x1, x0+x1
                     length+=1
-                res = max(res, length)
+                
+                if length > res:
+                    res = length
+        
         return res if res > 2 else 0
 
 
