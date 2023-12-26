b80c2a9a508933b269b30edb0d1a6067e4b9f77d
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-12-25 17:37:24 -0800

    Time: 1583 ms (99.69%) | Memory: 62.5 MB (5.30%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
@@ -1,13 +1,11 @@
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
         found = [False]*3
-        x,y,z = target
-
-        for a,b,c in triplets:
-            if not found[0] and x==a and y>=b and z>=c:
+        for triplet in triplets:
+            if not found[0] and target[0]==triplet[0] and target[1]>=triplet[1] and target[2]>=triplet[2]:
                 found[0] = True
-            if not found[1] and x>=a and y==b and z>=c:
+            if not found[1] and target[0]>=triplet[0] and target[1]==triplet[1] and target[2]>=triplet[2]:
                 found[1] = True
-            if not found[2] and x>=a and y>=b and z==c:
+            if not found[2] and target[0]>=triplet[0] and target[1]>=triplet[1] and target[2]==triplet[2]:
                 found[2] = True
         return all(found)
