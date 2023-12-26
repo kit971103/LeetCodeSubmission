abcb0fed0df9311c65c94d6448000803e0830075
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-12-25 17:05:04 -0800

    Time: 29 ms (97.48%) | Memory: 17.4 MB (5.04%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -6,29 +6,18 @@ class Solution:
         
         last = s[0]
         bound = 1
-        unbond = 1 if last == "1" or last == "2" else 0
+        unbound = 1 if last in ("1", "2") else 0
         
         for cur in islice(s,1,None):
-            old_b = bound
-            old_ub = unbond
-
             if cur == "0":
-                
-                if last in ("1", "2"):
-                    bound = old_ub
-                    unbond = 0
-                else:
+                if last not in ("1", "2"):
                     return 0
-            elif last == "1":
-                bound = old_b + old_ub
-                unbond = old_b
-            elif last =="2":
-                bound = old_b + (old_ub if cur in ("1","2","3","4","5","6") else 0)
-                unbond = old_b
+                bound = unbound
+                unbound = 0
+            elif last == "1" or (last =="2" and cur in ("1","2","3","4","5","6")):
+                bound += unbound
+                unbound = bound - unbound
             else:
-                bound = old_b
-                unbond = old_b
-            
+                unbound = bound
             last = cur
-        
         return bound
