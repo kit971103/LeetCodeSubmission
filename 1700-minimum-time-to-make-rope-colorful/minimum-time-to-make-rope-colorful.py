79f89cdfa44e77564b1c6843fcfff3660a647ed5
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-12-26 17:44:55 -0800

    Time: 753 ms (99.40%) | Memory: 26.1 MB (89.62%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.py
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.py
@@ -1,24 +1,14 @@
-def mincost(colors, neededTime):
-    last = start = None
-    recording = False
-    res = 0
-    for i, c in enumerate(colors):
-        if c == last and not recording:
-            start = i-1
-            recording = True
-        elif c != last and recording:
-            res += sum(neededTime[start: i])-max(neededTime[start: i])
-            recording = False
-        last = c
-    
-    if recording:
-        res += sum(neededTime[start:])-max(neededTime[start:])
-
-    return res
-
 class Solution:
     def minCost(self, colors: str, neededTime: List[int]) -> int:
-        return mincost(colors, neededTime)
+        timeSum = 0
 
-
+        for x in range(len(colors) - 1):
+            if colors[x] == colors[x + 1]:
+                if neededTime[x] < neededTime[x+1]:
+                    timeSum += neededTime[x]
+                else:
+                    timeSum += neededTime[x+1]
+                    neededTime[x+1] = neededTime[x]
+        #  print(timeSum)
         
+        return timeSum
