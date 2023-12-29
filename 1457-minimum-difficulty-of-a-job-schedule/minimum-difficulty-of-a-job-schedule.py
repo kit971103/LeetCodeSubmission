a832ae2d20b9dc6b28f7634507476cf1e41175b0
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-12-28 19:04:43 -0800

    Time: 77 ms (97.80%) | Memory: 17.4 MB (63.00%) - LeetSync

--- 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.py
+++ 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.py
@@ -1,25 +1,23 @@
 class Solution:
-    def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
-        if d > len(jobDifficulty): 
+    def minDifficulty(self, jobDifficulty, d):
+        n = len(jobDifficulty)
+        if n < d:
             return -1
-        
-        @cache
-        def dp(end, d):
-            if d == end:
-                return sum(jobDifficulty[:end])
-            elif d == 1:
-                return max(jobDifficulty[:end])
-
-            res = defalut
-            for start in range(d-1,end):
-                t = dp(start, d-1) + max(jobDifficulty[start:end])
-                if t < res:
-                    res = t
-            return res
-        
-        defalut = sum(jobDifficulty)
-        ans = dp(len(jobDifficulty), d)
-
-        return ans
-
+        prev = [float('inf')] * n
+        curr = [float('inf')] * n
+        for day in range(d):
+            stack = []
+            for i in range(day, n):
+                if i == 0:
+                    curr[i] = jobDifficulty[0]
+                else:
+                    curr[i] = prev[i - 1] + jobDifficulty[i]
+                while stack and jobDifficulty[stack[-1]] <= jobDifficulty[i]:
+                    j = stack.pop()
+                    curr[i] = min(curr[i], curr[j] + jobDifficulty[i] - jobDifficulty[j])
+                if stack:
+                    curr[i] = min(curr[i], curr[stack[-1]])
+                stack.append(i)
+            prev, curr = curr, prev
+        return prev[-1]
         
