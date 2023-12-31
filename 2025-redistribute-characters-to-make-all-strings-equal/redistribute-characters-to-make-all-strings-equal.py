056c348d6ea8032156f2e9181a2dcaba87e5f5cd
kit971103 <149559258+kit971103@users.noreply.github.com>
2023-12-31 00:26:53 -0800

    Time: 118 ms (5.35%) | Memory: 17.5 MB (6.29%) - LeetSync

--- 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.py
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.py
@@ -2,6 +2,8 @@ class Solution:
     def makeEqual(self, words: List[str]) -> bool:
         count = Counter(itertools.chain.from_iterable(words))
         n = len(words)
+        if count.total()%n:
+            return False
         for c in count:
             if count[c]%n:
                 return False
