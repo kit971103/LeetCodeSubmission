cab83ab215a8414764a12f7bacd874e39719c68c
kit971103 <149559258+kit971103@users.noreply.github.com>
2024-01-01 21:54:12 -0800

    Time: 60 ms (43.13%) | Memory: 17.2 MB (20.19%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.py
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.py
@@ -1,11 +1,11 @@
 class Solution:
     def findMatrix(self, nums: List[int]) -> List[List[int]]:
         cnt= collections.Counter(nums)
-        res= [[0] for _ in range(max(list(cnt.values()))) ]
-        for i in range(len(nums)):
+        res= [[] for _ in range(max(list(cnt.values()))) ]
+        for n in nums:
             j=0
-            while cnt[nums[i]]>0:
-                res[j].append(nums[i])
-                j=j+1
-                cnt[nums[i]]-=1
-        return [r[1:] for r in res]
+            while cnt[n]>0:
+                res[j].append(n)
+                j+=1
+                cnt[n]-=1
+        return res
