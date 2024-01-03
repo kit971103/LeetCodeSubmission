1ff07ce6b7bc9c619b01398232a48cfd3063ae0c
kit971103 <149559258+kit971103@users.noreply.github.com>
2024-01-02 16:30:02 -0800

    Time: 148 ms (37.48%) | Memory: 19.5 MB (6.86%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.py
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.py
@@ -1,11 +1,9 @@
 class Solution:
     def numberOfBeams(self, bank: List[str]) -> int:
-        floorDevs = []
-        totalBeam= 0
-        for floor in bank:
-            floorDev = floor.count("1")
-            if floorDev != 0: 
-                floorDevs.append(floorDev)
-        for i in range(len(floorDevs)-1):
-            totalBeam += floorDevs[i]*floorDevs[i+1]      
-        return totalBeam
+        res = decives_pre = 0
+        for row in bank:
+            decives_this = row.count("1")
+            if decives_this!=0:
+                res+=decives_pre*decives_this
+                decives_pre = decives_this
+        return res
