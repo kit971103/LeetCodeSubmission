8df1469cf9ea5038c0b06062efc2d29658b05d83
kit971103 <149559258+kit971103@users.noreply.github.com>
2024-01-04 12:05:46 -0800

    Time: 548 ms (94.44%) | Memory: 31.8 MB (12.65%) - LeetSync

--- 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.py
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.py
@@ -1,10 +1,5 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        res = 0
-        for f in Counter(nums).values():
-            if f == 1:
-                return -1
-            res += (f+2)//3
-        return res
+        return sum( (f+2)//3 for f in fs) if all(map(lambda x: x>1, fs:=Counter(nums).values())) else -1
 
         
