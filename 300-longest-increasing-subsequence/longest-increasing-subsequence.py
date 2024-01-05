80940a1336135993114c186e257b2ffd74e9265e
kit971103 <149559258+kit971103@users.noreply.github.com>
2024-01-04 22:12:04 -0800

    Time: 1165 ms (73.03%) | Memory: 17.6 MB (20.94%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -3,10 +3,7 @@ class Solution:
         dp = [0]*len(nums)
 
         for i in range(len(nums)):
-            try:
-                dp[i] = max(dp[j] for j in range(i) if nums[i] > nums[j])+1
-            except ValueError:
-                dp[i] = 1
+            dp[i] = max(itertools.chain([0], (dp[j] for j in range(i) if nums[i] > nums[j])) )+1
         return max(dp)
 
         
