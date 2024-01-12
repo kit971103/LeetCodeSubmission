2fb0cc375eeca4ffbaff20c284a0f6f0bb364f09
kit971103 <149559258+kit971103@users.noreply.github.com>
2024-01-12 00:23:14 -0800

    Time: 33 ms (94.64%) | Memory: 17.3 MB (19.92%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.py
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.py
@@ -1,13 +1,6 @@
 class Solution:
-    # vowels = "aeiouAEIOU"
-    vowels = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}
+    vowels = "aeiouAEIOU"
+    # vowels = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}
     def halvesAreAlike(self, s: str) -> bool:
-        res = 0
-        for c in islice(s, 0, len(s)//2):
-            if c in Solution.vowels:
-                res+=1
-        for c in islice(s, len(s)//2, None):
-            if c in Solution.vowels:
-                res-=1
-        return res == 0
+        return (sum( 1 for c in islice(s,0,len(s)//2) if c in Solution.vowels) - sum( 1 for c in islice(s,len(s)//2, None) if c in Solution.vowels) ) == 0
 
