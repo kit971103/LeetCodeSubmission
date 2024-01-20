8cd4d9dd69764e9c1c143872cdbc861b9c4496dd
kit971103 <149559258+kit971103@users.noreply.github.com>
2024-01-19 23:52:45 -0800

    Time: 114 ms (90.47%) | Memory: 17.1 MB (90.70%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.py
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -1,18 +1,8 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-        n = len(matrix)
         dp = matrix[0]
         for row in islice(matrix, 1, None):
-            t = []
-            for i, val in enumerate(row):
-                if 0 < i < n-1:
-                    k = min(dp[i-1:i+2])
-                elif i == 0:
-                    k = min(dp[i:i+2])
-                else:
-                    k = min(dp[i-1:i+1])
-                t.append(val+k)
-            dp = t.copy()
+            dp = [val + (min(dp[i-1:i+2]) if i else min(dp[0:2])) for i,val in enumerate(row)]
         return min(dp)
 
         
