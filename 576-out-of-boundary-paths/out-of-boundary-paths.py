9ab65c5954feada03dc4a8c23633985dc164034c
kit971103 <149559258+kit971103@users.noreply.github.com>
2024-01-25 23:59:45 -0800

    Time: 90 ms (70.52%) | Memory: 16.7 MB (91.91%) - LeetSync

--- 576-out-of-boundary-paths/out-of-boundary-paths.py
+++ 576-out-of-boundary-paths/out-of-boundary-paths.py
@@ -1,26 +1,14 @@
 class Solution:
     def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
-        res = 0
+        if maxMove == 0:
+            return 0
         dp = [[0]*n for _ in range(m)]
         dp[startRow][startColumn] = 1
+        res = sum(dp[0])+sum(dp[-1])+sum(dp[i][0] for i in range(m))+sum(dp[i][-1] for i in range(m))
         MOD=10**9+7
-        for _ in range(maxMove):
+        for _ in range(maxMove-1):
+            dp = [ [ (dp[row-1][col] if row > 0 else 0)+(dp[row+1][col] if row < m-1 else 0)+(dp[row][col-1] if col > 0 else 0)+(dp[row][col+1] if col < n-1 else 0) for col in range(n)] for row in range(m)]
             res += sum(dp[0])+sum(dp[-1])+sum(dp[i][0] for i in range(m))+sum(dp[i][-1] for i in range(m))
             if res > MOD:
                 res%=MOD
-            temp = [[0]*n for _ in range(m)]
-            for row, col in product(range(m), range(n)):
-                t=0
-                if row > 0:
-                    t += dp[row-1][col]
-                if row < m-1:
-                   t += dp[row+1][col]
-                if col > 0:
-                    t += dp[row][col-1]
-                if col < n-1:
-                    t += dp[row][col+1]
-                if t > MOD:
-                    t%=MOD 
-                temp[row][col] = t
-            dp = temp
         return res
