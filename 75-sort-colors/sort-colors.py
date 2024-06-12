84eeeb3c9f3ffb3e1f9b7cd784fb85c2f88a3386
kit971103 <149559258+kit971103@users.noreply.github.com>
2024-06-11 23:37:08 -0700

    Time: 44 ms (12.57%) | Memory: 16.4 MB (79.02%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -6,7 +6,6 @@ class Solution:
         c = collections.Counter(nums)
         i0 = c[0] - 1
         i1 = i0 + c[1]
-        i2 = i1 + c[2]
         for i in range(len(nums)):
             nums[i] = (i > i0) + (i > i1)
 
