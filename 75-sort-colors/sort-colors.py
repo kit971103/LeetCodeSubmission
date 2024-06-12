413da91ab140581e7b4216cd6ae09a3b5cfe21db
kit971103 <149559258+kit971103@users.noreply.github.com>
2024-06-11 23:39:20 -0700

    Time: 27 ms (97.72%) | Memory: 16.7 MB (7.83%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -4,10 +4,12 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         c = collections.Counter(nums)
-        i0 = c[0] - 1
+        i0 = c[0]
         i1 = i0 + c[1]
-        for i in range(len(nums)):
-            nums[i] = (i > i0) + (i > i1)
+        nums[0:i0] = [ 0 for _ in range(i0) ]
+        nums[i0:i1] = [ 1 for _ in range(i1-i0) ]
+        nums[i1:] = [ 2 for _ in range(len(nums)-i1) ]
+
 
 
 
