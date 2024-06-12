cad28bcc4339b996ab85aeafb10e4ab7aafa2506
kit971103 <149559258+kit971103@users.noreply.github.com>
2024-06-11 23:35:54 -0700

    Time: 44 ms (12.57%) | Memory: 16.6 MB (33.92%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -8,13 +8,7 @@ class Solution:
         i1 = i0 + c[1]
         i2 = i1 + c[2]
         for i in range(len(nums)):
-            if i <= i0:
-                n = 0
-            elif i <= i1:
-                n = 1
-            else:
-                n = 2
-            nums[i] = n
+            nums[i] = (i > i0) + (i > i1)
 
 
 
