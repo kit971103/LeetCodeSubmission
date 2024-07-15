cb0035398974ce64c09711b547f33caf7c0dfa65
kit971103 <149559258+kit971103@users.noreply.github.com>
2024-07-14 18:35:48 -0700

    Time: 40 ms (55.51%) | Memory: 16.6 MB (24.89%) - LeetSync

--- 1904-second-largest-digit-in-a-string/second-largest-digit-in-a-string.py
+++ 1904-second-largest-digit-in-a-string/second-largest-digit-in-a-string.py
@@ -1,7 +1,17 @@
 class Solution:
     def secondHighest(self, s: str) -> int:
-        try:
-            return sorted(list(set(map(int, filter(lambda c: c.isnumeric(), s)))))[-2]
-        except IndexError:
-            return -1
+        res = [None, None]
+        for n in map(int, filter(lambda c: c.isnumeric(), s)):
+            if res[0] is None:
+                res[0] = n
+            elif n > res[0]:
+                res[1] = res[0]
+                res[0] = n
+            elif n == res[0]:
+                continue
+            elif res[1] is None or n > res[1]:
+                res[1] = n
+        print(res)
+        return res[1] if res[1] is not None else -1
+                
         
