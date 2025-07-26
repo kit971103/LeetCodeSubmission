206720bff63d9f9f4c71429e8694041b7e4b8cee
Kit <kit971103@gmail.com>
2025-07-25 23:37:29 -0700

    Time: 725 ms (63.06%) | Memory: 87.2 MB (70.27%) - LeetSync

--- 3789-maximize-subarrays-after-removing-one-conflicting-pair/maximize-subarrays-after-removing-one-conflicting-pair.py
+++ 3789-maximize-subarrays-after-removing-one-conflicting-pair/maximize-subarrays-after-removing-one-conflicting-pair.py
@@ -1,17 +1,17 @@
 class Solution:
     def maxSubarrays(self, n: int, conflictingPairs: List[List[int]]) -> int:
-        
-        right = [[] for _ in range(n+1)]
-        for a,b in conflictingPairs:
-            right[max(a,b)].append(min(a,b))
-        
-        ans = 0 
-        left = [0, 0] 
-        bonus = [0] * (n + 1)
+        right = [ [] for _ in range(n+1)]
+        for pair in conflictingPairs:
+            right[max(pair)].append(min(pair))
+
+        left = [0, 0]
+        total = 0
+        bouns = [0] * (n + 1)
         for r in range(1, n + 1):
             for l in right[r]:
-                left = max(left, [l, left[0]], [left[0], l])
-            ans += r - left[0]
-            bonus[left[0]] += left[0] - left[1] 
-        
-        return ans + max(bonus)
+                left = max(left, [left[0], l], [l, left[0]])
+
+            total += r - left[0]
+            bouns[left[0]] += left[0] - left[1]
+
+        return total +max(bouns)
