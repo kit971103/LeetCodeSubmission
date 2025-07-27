553f9a1dce0dc20db6f7799e510fee8af0909b1e
Kit <kit971103@gmail.com>
2025-07-27 10:00:18 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (39.28%) - LeetSync

--- 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.py
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.py
@@ -1,20 +1,12 @@
 class Solution:
     def countHillValley(self, nums: List[int]) -> int:
-        # if only two number, there is no hill nor valley by definition
-        if len(nums) <= 2:
-            return 0
-
         total = 0
         left = nums[0]
-        for i, n in enumerate(nums[1:-1], start = 1):
-            right = nums[i + 1]
-            # print(f"{i=}, {left=}, {n=}, {right=}")
-
-            if (n > left and n > right) or (n < left and n < right):
-                # print(f"added at {i}")
+        for mid, right in itertools.pairwise(nums[1:]):
+            if (mid > left and mid > right) or (mid < left and mid < right):
                 total += 1
 
-            left = n if n != right else left
+            left = mid if mid != right else left
         return total
 
 
