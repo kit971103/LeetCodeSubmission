e17ea475cedb286edc9b0f46c6616222d57cde15
Kit <kit971103@gmail.com>
2025-07-29 21:33:46 -0700

    Time: 662 ms (70.15%) | Memory: 31.5 MB (18.66%) - LeetSync

--- 2498-smallest-subarrays-with-maximum-bitwise-or/smallest-subarrays-with-maximum-bitwise-or.py
+++ 2498-smallest-subarrays-with-maximum-bitwise-or/smallest-subarrays-with-maximum-bitwise-or.py
@@ -1,10 +1,33 @@
 class Solution:
     def smallestSubarrays(self, a: List[int]) -> List[int]:
-        return [*map(lambda d:max(d.values())-d[-1]+1,accumulate(range(len(a)-1,-1,-1),
-            lambda d,i:d|{q:i for q in range(32) if a[i]&1<<q}|{-1:i},initial={-1:0}))][:0:-1]
+        # max imput = 10^9 < 2^30, so 32 bit will be enough
+        bit_map = [[] for _ in range(32)]
 
-
-
-
-
+        # bulid the bit map
+        for index in range(len(a)-1, -1, -1):
+            n = a[index]
+            offset = 0
+            while n > 0:
+                if n%2:
+                    bit_map[offset].append(index)
+                n = n >> 1
+                offset+=1
+        # print(bit_map)
         
+        answer = []
+        for index in range(len(a)):
+            is_one = []
+            for indexes in bit_map:
+                if not indexes:
+                    continue
+                if indexes[-1] < index:
+                    indexes.pop()
+                if indexes:
+                    is_one.append(indexes[-1])
+            # print(f"{index=}, {is_one=}")
+            answer.append(max(is_one) - index + 1 if is_one else 1)
+        
+        return answer
+
+    
+                
