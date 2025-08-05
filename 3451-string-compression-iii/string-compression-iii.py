59c0374fe822bbfee768395beebc3aa989d9b04d
Kit <kit971103@gmail.com>
2025-08-04 17:31:30 -0700

    Time: 211 ms (62.72%) | Memory: 28.5 MB (24.56%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.py
+++ 3451-string-compression-iii/string-compression-iii.py
@@ -1,19 +1,10 @@
 class Solution:
     def compressedString(self, word: str) -> str:
-        result:list[str] = []
-        count = 0
-        rep = word[0]
-        
-        for c in word:
-            if c == rep:
-                if count == 9:
-                    count = 1
-                    result.append(f"9{rep}")
-                else:
-                    count += 1
-            else:
-                result.append(f"{count}{rep}")
-                rep = c
-                count = 1
-        result.append(f"{count}{rep}")
+        result = []
+        for c, group in itertools.groupby(word):
+            length = len(list(group))
+            result.extend(f"9{c}" for _ in range(length // 9))
+            r = length % 9
+            if r:
+                result.append(f"{r}{c}")
         return "".join(result)
